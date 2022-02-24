dbe5a9a0bd16f7bab672b251c797dc06ee15f565
Jhonatanmizu <natanjesuss20@gmail.com>
2022-02-23 22:28:00 -0300

    started slider

---
+++ src/components/HomeComponents/Slider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style></style>
