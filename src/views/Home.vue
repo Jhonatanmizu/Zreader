d744503b0e816f9b6cd343066e241690eeb018ee
Jhonatanmizu <natanjesuss20@gmail.com>
2022-02-24 23:08:54 -0300

    make slider style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <Main />
   <HowItWork />
+  <Slider />
   <Proposal />
 </template>
 
@@ -8,12 +9,14 @@
 import HowItWork from "../components/HomeComponents/HowItWork.vue";
 import Main from "../components/HomeComponents/Main.vue";
 import Proposal from "../components/HomeComponents/Proposal.vue";
+import Slider from "../components/HomeComponents/Slider.vue";
 export default {
   name: "Home",
   components: {
     Main,
     HowItWork,
     Proposal,
+    Slider,
   },
 };
 </script>
