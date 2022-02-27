047db324747b2b27bcfa98d40fa2d68dad447f80
Jhonatanmizu <natanjesuss20@gmail.com>
2022-02-27 17:40:01 -0300

    started about view

---
+++ src/components/AboutComponents/Theory.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <div class="container gap">
+      <h3>Um pouco mais de teoria</h3>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Sobre o projeto</h1>
+    <Theory />
   </div>
 </template>
+<script>
+import Theory from "../components/AboutComponents/Theory.vue";
+
+export default {
+  name: "About",
+  components: {
+    Theory,
+  },
+};
+</script>
