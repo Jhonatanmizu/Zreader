cc3db2fc5917ce1f8bf6fc32fd20c6fb01b7271b
Jhonatanmizu <natanjesuss20@gmail.com>
2022-02-27 19:21:34 -0300

    finished theory component

--- src/components/AboutComponents/Theory.vue
+++ src/components/AboutComponents/Theory.vue
@@ -1,14 +1,38 @@
 <template>
-  <section>
-    <div class="container gap">
-      <h3>Um pouco mais de teoria</h3>
+  <section class="theory-section">
+    <div class="container gap theory-content">
+      <h3 class="theory-title mb24">Um pouco mais de teoria</h3>
+      <p class="theory-text mb24">
+        O zReader é resultado de um amplo estudo sobre o uso de tecnologia
+        digital para apoiar a alfabetização de crianças. Desenvolvido após a
+        observação dos problemas causados pelo uso tecnologias digitais durante
+        ciclo de alfabetização,
+        <strong>
+          baseia-se nos fundamentos da teoria construcionista de Seymour Papert
+          que, por sua vez é baseada no construcionismo de Jean Piaget.</strong
+        >
+        De forma resumida, o construtivismo enfatiza a interação do sujeito com
+        o objeto de estudo como um meio para alcançar a aprendizagem. O
+        construcionismo direciona essa interação para a criação de artefatos,
+        que no caso do zReader são as animações.
+      </p>
+      <p class="theory-text">
+        No zReader, essas interações ocorrem durante a leitura e durante a
+        criação da animação final, no zStudio. Durante a leitura dos zBooks, as
+        interações ocorrem pela leitura das páginas e pela a escolha de
+        representações visuais ou palavras adequadas para um segmento de texto.
+        No zStudio, as interações ocorrem pela leitura dos textos nos balões das
+        animações, pela criação improvisada de falas dos personagens e/ou
+        narração da história , pela entonação na leitura ou criação das falas
+        dos personagens, e pela montagem das animações na sequência correta.
+      </p>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: "Test",
+  name: "Theory",
   created() {},
   data() {
     return {};
@@ -18,4 +42,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.theory-section {
+  margin-bottom: 3.75rem;
+}
+.theory-content .theory-title {
+  text-align: center;
+}
+.theory-content .theory-text {
+  text-align: justify;
+  line-height: 1.875rem;
+}
+</style>
