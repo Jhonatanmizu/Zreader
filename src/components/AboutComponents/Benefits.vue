685e8ca6fef063dc24f4ed42f51357e25be48676
Jhonatanmizu <natanjesuss20@gmail.com>
2022-03-01 20:24:41 -0300

    Fixed benefits text

--- src/components/AboutComponents/Benefits.vue
+++ src/components/AboutComponents/Benefits.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container benefits-content gap">
-    <h4 class="benefits-title">
+    <h3 class="benefits-title">
       Os principais benefícios para as crianças são melhorias em relação à:
-    </h4>
+    </h3>
     <ul class="benefits-list mb24">
       <li>Habilidades de leitura</li>
       <li>Capacidade de interpretação de textos</li>
       <li>Atenção durante a leitura;</li>
-      <li>entonação e pontuação (prosódia);</li>
-      <li>criatividade</li>
-      <li>capacidade de improvisação.</li>
+      <li>Entonação e pontuação (prosódia);</li>
+      <li>Criatividade</li>
+      <li>Capacidade de improvisação.</li>
     </ul>
     <p class="benefits-text">
       Para detalhes dos aspectos teóricos e experimentos que demonstram
@@ -45,6 +45,7 @@ export default {
 .benefits-content .benefits-text {
   text-align: justify;
 }
+
 .benefits-list li {
   list-style-type: disc;
 }
